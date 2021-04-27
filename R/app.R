1fbea8d8558133c843d945ad51713f73d474ac6d
Ozan Cetin <cetin.ozanahmet@gmail.com>
2021-04-28 01:30:51 +0300

    shiny app created with bare bones

--- R/app.R
+++ R/app.R
@@ -1,18 +1,11 @@
 library(shiny)
 
-milex <- read.csv("./data/data.csv")
-data_description <- read.csv("./data/data_descriptions_modified.csv")
 ui <- fluidPage(
-  tabPanel("Univariate",
-           ),
-  tabPanel("Bivariate",
-           ),
-  tabPanel("Spreadsheet",
-           )    
+  
 )
 
 server <- function(input, output, session) {
-    
+  
 }
 
 shinyApp(ui, server)
